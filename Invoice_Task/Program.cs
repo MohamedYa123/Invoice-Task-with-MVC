882429dadc53b0368cdbbd72bff1ee4c2b27c323
Mohamed Yasser <tecyouth123@gmail.com>
2023-10-28 14:52:07 +0200

    Added automatic fill if the database is empty

--- Invoice_Task/Program.cs
+++ Invoice_Task/Program.cs
@@ -1,9 +1,35 @@
+using Invoice_Task.Database;
+
 namespace Invoice_Task
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var db = new dbManager();
+            var customerscount=db.Customers.Count();
+            if (customerscount == 0)
+            {
+                for(int i=0;i<5; i++)
+                {
+                    Customer customer = new Customer();
+                    customer.Name = $"Customer {i+1}";
+                    db.Customers.Add(customer);
+                }
+            }
+            var itemscount=db.Items.Count();
+            if (itemscount == 0)
+            {
+                for(int i=0;i<5; i++)
+                {
+                    Item item = new Item();
+                    item.Name = $"Item {i+1}";
+                    item.price = (i + 1) * 10;
+                    item.code = i;
+                    db.Items.Add(item); 
+                }
+            }
+            db.SaveChanges();
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
